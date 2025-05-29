36b41fcf2bc98fa71776abb680a2e7a1639b1409
Josh Vote <jjvote@gmail.com>
2025-05-29 14:01:56 +1000

    Tagging v5

--- docker/cactus-teststack-init/entrypoint.sh
+++ docker/cactus-teststack-init/entrypoint.sh
@@ -15,28 +15,4 @@ done
 echo "Running migrations..."
 psql ${ENVOY_DATABASE_URL} -f /migrate.sql
 
-# Watchdog - blocks until file is created
-echo "(2) cactus-envoy Setup"
-
-if [ -z "$KICKOFF_FILE" ]; then
-  echo "Error: KICKOFF_FILE environment variable not set."
-  exit 1
-fi
-
-WATCHED_DIR=$(dirname "$KICKOFF_FILE")
-
-# Ensure the directory exists
-if [ ! -d "$WATCHED_DIR" ]; then
-  echo "Error: Directory $WATCHED_DIR does not exist."
-  exit 1
-fi
-
-echo "Blocking until $KICKOFF_FILE is found..."
-
-while true; do
-  inotifywait -e create --include $(basename $KICKOFF_FILE) "$WATCHED_DIR"
-
-  echo "Detected $KICKOFF_FILE at $(date)"
-  break
-
-done
+echo "End of teststack-init"
